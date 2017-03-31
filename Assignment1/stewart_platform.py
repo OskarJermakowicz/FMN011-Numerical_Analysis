2c14ebd1185ad3b1b03fc14f3bec55821d5fbdee
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2017-03-31 13:17:58 +0200

    Plot of base and upper frame done

--- Assignment1/stewart_platform.py
+++ Assignment1/stewart_platform.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math
 import scipy.optimize as sci
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import matplotlib.pyplot as plt
 
 # Returns a list of three elements, for each i, which includes h, x and y for each.
 def geval(L, b, d):
@@ -10,7 +13,7 @@ def geval(L, b, d):
         P_i = (1/(2.0*b))*(b**2 + L[2*i-2]**2 - L[2*i-1]**2)
         h_i = math.sqrt(L[2*i-2]**2 - P_i**2)
         res.append([h_i, 0, 0])
-    # print(res)
+
     # XP1 and YP1
     res[0][1] = (math.sqrt(3.0) / 6.0) * (2.0 * b + d - 3.0 * P_i)
     res[0][2] = (1.0 / 2.0) * (d + P_i)
@@ -28,15 +31,6 @@ def geval(L, b, d):
 # Determines the left hand side of equations for calculating the X-coordinates of the vertices in the upper frame. Ideally the left hand side should equal zero.
 def stf(xt, a, gev):
     equations = []
-    print("p1",gev[0][1])
-    print("p2",gev[1][1])
-    print("p3",gev[2][1])
-    print("h1",gev[0][0])
-    print("h2",gev[1][0])
-    print("h3",gev[2][0])
-    print("y1",gev[0][2])
-    print("y2",gev[1][2])
-    print("y3",gev[2][2])
     equations.append(a**2 + 2.0*xt[0]*xt[1] - 2.0*xt[0]*(gev[0][1] + math.sqrt(3.0)*(gev[0][2]-gev[1][2])) - 2.0*gev[1][1]*xt[1] - ((math.sqrt(3.0)*gev[0][1]-gev[0][2] + gev[1][2])**2 + (gev[0][0]**2 + gev[1][0]**2) - 4.0*gev[0][1]**2 - gev[1][1]**2) + 2.0*math.sqrt((gev[0][0]**2 - 4.0*(xt[0]-gev[0][1])**2)*(gev[1][0]**2 - (xt[1] - gev[1][1])**2)))
     equations.append(a**2 - 4.0*xt[0]*xt[2] - 2.0*xt[0]*(gev[0][1] - 3.0*gev[2][1] + math.sqrt(3.0)*(gev[0][2]-gev[2][2])) - 2.0*xt[2]*((-3.0)*gev[0][1] + gev[2][1] + math.sqrt(3.0)*(gev[0][2] - gev[2][2])) - ((math.sqrt(3.0)*(gev[0][1] + gev[2][1]) - gev[0][2] + gev[2][2])**2 + (gev[0][0]**2 + gev[2][0]**2) - 4.0*gev[0][1]**2 - 4.0*gev[2][1]**2) + 2.0*math.sqrt((gev[0][0]**2 - 4.0*(xt[0] - gev[0][1])**2)*(gev[2][0]**2 - 4.0*(xt[2] - gev[2][1])**2)))
     equations.append(a**2 + 2.0*xt[1]*xt[2] - 2.0*xt[2]*(gev[2][1] + math.sqrt(3.0)*(gev[1][2] - gev[2][2])) - 2.0*gev[1][1]*xt[1] - ((math.sqrt(3.0)*gev[2][1] - gev[1][2] + gev[2][2])**2 + (gev[1][0]**2 + gev[2][0]**2) - gev[1][1]**2 - 4.0*gev[2][1]**2) + 2.0*math.sqrt((gev[1][0]**2 - (xt[1] - gev[1][1])**2)*(gev[2][0]**2 - 4.0*(xt[2] - gev[2][1])**2)))
@@ -110,7 +104,7 @@ def task3():
     a = 10
 
     # Starting point of the X-coordinates
-    xt_start = [3, -6, 3]
+    xt_start = [2.5, -5.5, 2.5]
 
     # Solve for xt
     xt = sci.fsolve(stf, xt_start, (a, geval(L, b, d)))
@@ -131,6 +125,17 @@ def task5():
     zt = zt_coords(xt, geval(L, b, d))
     base_x = base_x_coords(b, d)
     base_y = base_y_coords(b, d)
+    base_z = [0,0,0,0,0,0]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(xt, yt, zt, color='black')
+    ax.plot_trisurf(base_x, base_y, base_z, color='blue')
+    x_label = ax.set_xlabel('\nX')
+    y_label = ax.set_ylabel('\nY')
+    z_label = ax.set_zlabel('\nZ')
+
+    plt.show()
 
 print("-- Task 1:")
 task1()
@@ -143,4 +148,3 @@ task3()
 
 print("\n-- Task 5:")
 task5()
-
