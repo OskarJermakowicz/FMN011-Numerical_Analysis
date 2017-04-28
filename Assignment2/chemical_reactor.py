19fb42c054c2d7a46efc85245b20cfdfb46e951f
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2017-04-28 11:55:01 +0200

    Started assignment 2

---
+++ Assignment2/chemical_reactor.py
@@ -0,0 +1,14 @@
+import numpy as np
+import math
+import scipy.optimize as sci
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import matplotlib.pyplot as plt
+
+def task1():
+    k = 0.01
+    depth = [0, 0.5, 1, 1.5, 2, 2.5, 3]
+    temp = [70, 70, 55, 22, 13, 10, 10]
+
+print("-- Task 1:")
+task1()
