0719999a7cb88a2b98e083b711745359cf8e8b33
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2017-04-29 11:53:13 +0200

    Task 1 done

--- Assignment2/chemical_reactor.py
+++ Assignment2/chemical_reactor.py
@@ -1,14 +1,18 @@
 import numpy as np
-import math
-import scipy.optimize as sci
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
 def task1():
     k = 0.01
     depth = [0, 0.5, 1, 1.5, 2, 2.5, 3]
     temp = [70, 70, 55, 22, 13, 10, 10]
+
+    cs = CubicSpline(depth, temp, bc_type='clamped')
+    x = np.linspace(0,3)
+    plt.plot(depth, temp, 'o', label='data')
+    plt.plot(x, cs(x), 'r', label='Interpolated curve')
+    plt.legend(loc='lower left', ncol=2)
+    plt.show()
 
 print("-- Task 1:")
 task1()
