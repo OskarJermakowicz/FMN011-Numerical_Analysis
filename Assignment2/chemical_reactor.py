2f6d22ae82d19e060c047cebff5cf5d104156806
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2017-04-30 13:13:38 +0200

    Task 4 done, 5 almost done

--- Assignment2/chemical_reactor.py
+++ Assignment2/chemical_reactor.py
@@ -11,18 +11,30 @@ cs = CubicSpline(depth, temp, bc_type='clamped')
 x = np.linspace(0,3)
 
 # Task 1
-# plt.plot(depth, temp, 'o', label='data')
-# plt.plot(x, cs(x), label='Interpolated curve')
+plt.plot(depth, temp, 'o', label='data')
+plt.plot(x, cs(x), label='Interpolated curve')
 
 # Task 2
 tdepth = sci.fsolve(cs, 1, args=2, xtol=10e-6)
-plt.axvline(x=tdepth, linestyle='dashdot', label='Thermocline depth', lw=0.5)
+#plt.axvline(x=tdepth, linestyle='dashdot', label='Thermocline depth', lw=0.5)
 
 # Task 3
-plt.plot(x,-cs(x,1),label='Flux')
+#plt.plot(x,-cs(x,1),label='Flux')
+
+# Task 4
+#plt.plot(x, cs(x, 1), 'b', label='First derivative')
+#plt.plot(x, cs(x, 2), 'r', label='Second derivative')
+
+# Task 5
+t = sci.fsolve(cs, 1.07874, args=2, xtol=10e-6)
+print("Approximation of depth at 50C:", t[0])
+print("Approximation of temperature at 1.7m:", cs(1.7))
+
+plt.axvline(x=1.7, linestyle='dashdot', label='Approx. of 1.7m', lw=0.5, color='g')
+plt.axhline(y=50, linestyle='dashdot', label='Approx. of 50C', lw=0.5)
 
 # Show plot
 plt.legend(loc='lower left', ncol=2)
-x_label = plt.xlabel('\nDepth (centimeter)')
+x_label = plt.xlabel('\nDepth (meter)')
 y_label = plt.ylabel('\nTemperature (celsius)')
 plt.show()
