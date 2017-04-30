8dece1b63a6aff1e15b8234096ab40d4af720337
Oskar Jermakowicz <oskar.jermakowicz@gmail.com>
2017-04-30 12:16:40 +0200

    Task 2 and 3 done

--- Assignment2/chemical_reactor.py
+++ Assignment2/chemical_reactor.py
@@ -1,18 +1,28 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
+import scipy.optimize as sci
 
-def task1():
-    k = 0.01
-    depth = [0, 0.5, 1, 1.5, 2, 2.5, 3]
-    temp = [70, 70, 55, 22, 13, 10, 10]
+k = 0.01
+depth = [0, 0.5, 1, 1.5, 2, 2.5, 3]
+temp = [70, 70, 55, 22, 13, 10, 10]
 
-    cs = CubicSpline(depth, temp, bc_type='clamped')
-    x = np.linspace(0,3)
-    plt.plot(depth, temp, 'o', label='data')
-    plt.plot(x, cs(x), 'r', label='Interpolated curve')
-    plt.legend(loc='lower left', ncol=2)
-    plt.show()
+cs = CubicSpline(depth, temp, bc_type='clamped')
+x = np.linspace(0,3)
 
-print("-- Task 1:")
-task1()
+# Task 1
+# plt.plot(depth, temp, 'o', label='data')
+# plt.plot(x, cs(x), label='Interpolated curve')
+
+# Task 2
+tdepth = sci.fsolve(cs, 1, args=2, xtol=10e-6)
+plt.axvline(x=tdepth, linestyle='dashdot', label='Thermocline depth', lw=0.5)
+
+# Task 3
+plt.plot(x,-cs(x,1),label='Flux')
+
+# Show plot
+plt.legend(loc='lower left', ncol=2)
+x_label = plt.xlabel('\nDepth (centimeter)')
+y_label = plt.ylabel('\nTemperature (celsius)')
+plt.show()
